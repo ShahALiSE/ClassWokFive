6e3f1a6ed860e4bb19a9d3b642bf487553feec6c
Shah ALi <untold352@gmail.com>
2018-07-25 03:32:08 +0000

    classwork8

--- server.js
+++ server.js
@@ -7,6 +7,31 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 var server = http.Server(app);
 
+var mongo =require('mongodb');
+//for local
+//var db,uri, ="mongodb://localhost":27017";
+
+//for c9
+var db, uri ="mongodb://"+process.env.IP+":27017";
+
+mongo.MongoClient.connect(uri,
+  {useNewUrlParser:true},
+    function(err,client){
+      if (err){
+        console.log("could not connect o MongoDB");
+      }else{
+        db=client.db('simple-node');
+      }
+})
+
+
+
+var save = function(form_data){
+  db.createCollection('users',function(err,collection){});
+  var collection =db.collection('users');
+  collection.save(form_data);
+  
+}
 
 app.get('/',function(req,res){
   res.sendFile(__dirname+'/index.html');
@@ -19,7 +44,9 @@ app.get('/form',function(req,res){
 })
 
 app.post('/submit_user',function(req,res){
-    console.log(req.body);     
+    console.log(req.body);
+    save(req.body);
+    res.status('200');
 }) 
 
   server.listen(process.env.PORT, process.env.IP, function(){
