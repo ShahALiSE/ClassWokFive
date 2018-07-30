edb5196788ba399b1bd500d8e7f6f6f55a68f7ef
Shah ALi <untold352@gmail.com>
2018-07-30 03:19:49 +0000

    classwok-9

--- server.js
+++ server.js
@@ -2,52 +2,76 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://"+process.env.IP+":2701/node-cw8");
+
+mongoose.connection.on('error',function(){
+  console.log('could not connect to mongoose');
+});
+
+//define usr schema with mongoose
+
+var userSchema = new mongoose.Schema({
+  name:{
+    type:String,
+    required:"Name is required"
+  },
+    
+    email:String
+});
+
+var User = mongoose.model('User',userSchema);
+
+//var mongo = require('mongodb');
+//for local
+//var db, uri = "mongodb://localhost:27017";
+//for c9
+//var db, uri = "mongodb://"+process.env.IP+":27017";
+//mongo.MongoClient.connect(uri,
+  //      {useNewUrlParser:true},
+    //  function(err, client ){
+      //  if(err){
+        //  console.log('Could not connect to MongoDB')
+        //}else{
+          //db = client.db('node-cw8');
+        //}
+      //})
+//var save = function(form_data){
+  //db.createCollection('users', function(err, collection){});
+  //var collection = db.collection('users');
+  //collection.save(form_data);
+//}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 var server = http.Server(app);
 
-var mongo =require('mongodb');
-//for local
-//var db,uri, ="mongodb://localhost":27017";
-
-//for c9
-var db, uri ="mongodb://"+process.env.IP+":27017";
-
-mongo.MongoClient.connect(uri,
-  {useNewUrlParser:true},
-    function(err,client){
-      if (err){
-        console.log("could not connect o MongoDB");
-      }else{
-        db=client.db('simple-node');
-      }
-})
-
-
-
-var save = function(form_data){
-  db.createCollection('users',function(err,collection){});
-  var collection =db.collection('users');
-  collection.save(form_data);
-  
-}
-
-app.get('/',function(req,res){
+app.get('/', function(req, res){
   res.sendFile(__dirname+'/index.html');
-})
-app.get('/about',function(req,res){
-  res.sendFile(__dirname+'/about.html');
-})
-app.get('/form',function(req,res){
-  res.sendFile(__dirname+'/form.html');
-})
+});
 
-app.post('/submit_user',function(req,res){
-    console.log(req.body);
-    save(req.body);
-    res.status('200');
-}) 
+app.get('/form', function(req, res){
+  res.sendFile(__dirname+'/form.html');
+});
+
+app.post('/submit_user', function(req, res){
+  console.log(req.body);
+  var new_user = new User(req.body);
+  new_user.save(function(err,data){
+    if(err)
+    return res.status(400)
+              .json({message:"could not save user"});
+    res.status(200).json(data);
+  })
+  
+ // save(req.body);
+//  res.status(200);
+});
+
+app.get('/system/about', function(req, res){
+  res.sendFile(__dirname+'/about.html');
+});
 
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
